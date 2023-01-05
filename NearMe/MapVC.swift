975df2dc2fda44af037d91f2d24289fed7420995
Andrew <andrewmh.swift@gmail.com>
2023-01-05 18:04:39 -0500

    project setup

---
+++ NearMe/MapVC.swift
@@ -0,0 +1,16 @@
+//
+//  MapVC.swift
+//  NearMe
+//
+//  Created by Andrew H on 1/5/23.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
